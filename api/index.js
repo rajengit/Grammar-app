c5583c61489058328f8d719b8d86fa926eb39351
rajen <rajendra20597@gmail.com>
2024-03-17 14:08:04 +0530

    Adding ejs files

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get('/home',(req,res)=>{
+    res.render('home');
+})
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
